139c9b3ff1de607b592d5c4d140af504598a3043
losfair <zhy20000919@hotmail.com>
2018-03-09 23:01:48 +0800

    Add benches

--- benches/simple.rs
+++ benches/simple.rs
@@ -44,12 +44,12 @@ fn bench_run(b: &mut Bencher) {
 }
 
 #[bench]
-fn bench_sched(b: &mut Bencher) {
+fn bench_sched_run(b: &mut Bencher) {
     let mut sched = Scheduler::new_default();
-    let state = sched.state.clone();
+    let state = sched.get_state();
 
     b.iter(|| {
-        let vp = sched.state.run_coroutine(move |_| {
+        let vp = state.run_coroutine(move |_| {
         });
         let ret = sched.run_value_promise_to_end(vp);
         match ret {
@@ -57,4 +57,42 @@ fn bench_sched(b: &mut Bencher) {
             Err(e) => resume_unwind(e)
         }
     });
+}
+
+#[bench]
+fn bench_sched_yield(b: &mut Bencher) {
+    let mut sched = Scheduler::new_default();
+    let state = sched.get_state();
+
+    let b = unsafe {
+        ::std::mem::transmute::<&mut Bencher, &'static mut Bencher>(b)
+    };
+
+    let vp = state.run_coroutine(move |c| {
+        b.iter(|| {
+            let p = Promise::new_resolved();
+            c.yield_now(&p);
+        });
+    });
+    sched.run_value_promise_to_end(vp).unwrap();
+}
+
+#[bench]
+fn bench_sched_async(b: &mut Bencher) {
+    let mut sched = Scheduler::new_default();
+    let state = sched.get_state();
+
+    let b = unsafe {
+        ::std::mem::transmute::<&mut Bencher, &'static mut Bencher>(b)
+    };
+
+    let vp = state.run_coroutine(move |c| {
+        b.iter(|| {
+            let p = Promise::new(|cb| {
+                cb.call(());
+            });
+            c.yield_now(&p);
+        });
+    });
+    sched.run_value_promise_to_end(vp).unwrap();
 }
